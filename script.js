ecc7c04abfc28e871804a88151ef184dc0ad931c
julian87nicolas <julicmrgo@gmail.com>
2022-03-30 15:39:41 -0300

    OS list added with searcher

--- script.js
+++ script.js
@@ -17,3 +17,56 @@ window.addEventListener('scroll', () => {
     }
     console.log(window.pageYOffset);
 });
+
+const OS = ["Avalian", "America Servicios", "Caja forense", "CIMESA", "Conferencia episcopal argentina", "DASUTEN", "DAMSU", "Docthos", "Federada Salud", "Galeno", "Gerdanna Salud", "HOPE", "Jerarquicos Salud", "Luis Pasteur", "Medicus", "OPDEA", "OSDIPP", "OSJERA", "OSPIL", "OSPJN", "OSSACRA", "OSTV", "OSAPM", "OSSEG", "Policia Federal", "Prevencion Salud", "Swiss Medical", "Sancor Salud", "SCIS", "SADAIC", "Unimed"];
+
+let OSlist = "<ul>";
+
+for(let i=0;i<OS.length;i++){
+    OSlist += `<li>${OS[i]}</li>`;
+}
+OSlist += "</ul>";
+
+const OSlistButton = document.querySelector('.obra-social');
+
+const OSlistHTML = document.querySelector('.lista');
+
+OSlistButton.addEventListener('click', (e) => {
+        if(OSlistHTML.innerHTML == ""){
+            OSlistHTML.innerHTML = OSlist;
+            OSlistHTML.style.overflowY = "scroll";
+            OSlistHTML.style.height = "300px";
+
+            console.log(OSlistButton.offsetWidth);
+
+            OSlistButton.style.borderRadius = "2.5rem";
+        }
+        else{
+            OSlistHTML.innerHTML = "";
+            OSlistHTML.style.overflow = "hidden";
+            OSlistHTML.style.height = "0";
+
+            OSlistButton.style.borderRadius = "5rem";
+        }
+});
+
+const search = document.querySelector('#search');
+
+search.addEventListener("keyup", (e) => {
+    
+        let results = "<ul>";
+        for(let i=0;i<OS.length;i++){
+            if(OS[i].toLowerCase().includes(search.value.toLowerCase())){
+                results += `<li>${OS[i]}</li>`;
+            }
+        }
+        results += "</ul>";
+        OSlistHTML.innerHTML = results;
+        OSlistHTML.style.overflowY = "scroll";
+        OSlistHTML.style.height = "300px";
+
+        console.log(OSlistButton.offsetWidth);
+
+        OSlistButton.style.borderRadius = "2.5rem";
+    
+} );
